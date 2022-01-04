47fa87f72868d153ee06d228579deac663569e2e
Shafiul-Sabbir <sabbirvai82@gmail.com>
2022-01-04 18:21:41 +0600

    changes

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWQywCuZBmwQ4aCNvWhiLBTKzxnhflFns",
+  authDomain: "mega-project-2da90.firebaseapp.com",
+  projectId: "mega-project-2da90",
+  storageBucket: "mega-project-2da90.appspot.com",
+  messagingSenderId: "810148128139",
+  appId: "1:810148128139:web:b913d2871f377f8a4cbb8e"
+   };
+
+export default firebaseConfig;
